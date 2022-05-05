4b8e2617afdf85620eb26f017423c55e9178c6cc
shahnmah <shahn.mah@gmail.com>
2022-05-05 22:31:12 +0600

    header-added

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <div className="container">
+                
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
