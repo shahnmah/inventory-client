238cf7aa56e1c96a9330bf2139fe0ce432c12d8f
shahnmah <shahn.mah@gmail.com>
2022-05-06 01:00:25 +0600

    email signup added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [displayError, setDisplayError] = useState('')
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
     return (
         <div className='container user-login my-5'>
             <Form className='w-25 mx-auto text-center'>
                 <img className='my-3' src="https://i.ibb.co/mhLPrwR/logo-bus-removebg-preview.png" alt="" />
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control  onBlur={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3 text-start" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" />
-                    <a role="button" className='text-muted'>Forgot Password ?</a>
+                <Form.Control  onBlur={(e) => setPassword(e.target.value)} type="password" placeholder="Password" required />
                 </Form.Group>
                 
                 <Button className='w-100' variant="primary" type="submit">
@@ -23,7 +32,7 @@ const Login = () => {
                 <Button className='w-100 my-3' variant="primary" type="submit">
                     Google Login
                 </Button>
-                <a role="button"  className='text-muted'>Don't have an account ?</a>
+                <Link className='text-decoration-none' to='/register'>Don't have an account ?</Link>
             </Form>
         </div>
     );
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,33 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
 
 const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [displayError, setDisplayError] = useState('')
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+      const navigate= useNavigate()
+    const handleUserRegister = (e) =>{
+        e.preventDefault();
+        createUserWithEmailAndPassword(email, password)
+    }
+    if(user){
+        navigate('/login')
+    }
+    if(error){
+        setDisplayError(error.message)
+    }
     return (
         <div>
             <div className='container user-login my-5'>
-            <Form className='w-25 mx-auto text-center'>
-                <img className='my-3' src="https://i.ibb.co/mhLPrwR/logo-bus-removebg-preview.png" alt="" />
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="text" placeholder="Enter Name" />
-                </Form.Group>
+                <Form className='w-25 mx-auto text-center' onSubmit={handleUserRegister}>
+                    <img className='my-3' src="https://i.ibb.co/mhLPrwR/logo-bus-removebg-preview.png" alt="" />
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control onBlur={(e) => setName(e.target.value)} type="text" placeholder="Enter Name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control  onBlur={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" required />
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
-
-                <Form.Group className="mb-3 text-start" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                
-                <Button className='w-100' variant="primary" type="submit">
-                    Register
-                </Button>
-                <Button className='w-100 my-3' variant="primary" type="submit">
-                    Google Login
-                </Button>
-                <a role="button" className='text-muted'>Already have an account ?</a>
-            </Form>
-        </div>
+                    <Form.Group className="mb-3 text-start" controlId="formBasicPassword">
+                        <Form.Control  onBlur={(e) => setPassword(e.target.value)} type="password" placeholder="Password" required />
+                    </Form.Group>
+                    <p>{displayError}</p>
+                    <Button className='w-100' variant="primary" type="submit">
+                        Register
+                    </Button>
+                    <Button className='w-100 my-3' variant="primary" type="submit">
+                        Google Login
+                    </Button>
+                    <Link className='text-decoration-none' to='/login'>Don't have an account ?</Link>
+                </Form>
+            </div>
         </div>
     );
 };
