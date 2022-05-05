0e532c302893ee36d6f52ee9f5b43405dc804dad
shahnmah <shahn.mah@gmail.com>
2022-05-05 22:53:31 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div >
     <Header></Header>
+    <Home></Home>
     </div>
   );
 }
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import './banner.css';
 const Banner = () => {
     return (
         <div className='banner'>
-            <div className="container">
-                
+            <div className="container h-100 d-flex align-items-center justify-content-center">
+                <div>
+                    <h1 className='text-white'>Manage you all Buses</h1>
+                    <button className='btn btn-primary'>Manege Buses</button>
+                </div>
             </div>
         </div>
     );
