3aebaf3358e18dedc082296d5582ce076ed34e3e
shahnmah <shahn.mah@gmail.com>
2022-05-09 20:14:04 +0600

    slider added

--- src/components/Brands/Brands.js
+++ src/components/Brands/Brands.js
@@ -1,28 +1,46 @@
 import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const Brands = () => {
-    return (
-        <div>
-            <h2>Our Brands</h2>
-            <div className='bg-light'>
-                <div className="container">
-                    <div className="row py-5">
-                        <div className="col-lg-3 col-md-3 col-sm-4 col-6">
-                            <img src="https://i.ibb.co/Tghj6Zx/pngwing-com.png" alt="" />
-                        </div>
-                        <div className="col-lg-3 col-md-3 col-sm-4 col-6">
-                            <img src="https://i.ibb.co/5K26qMT/pngwing-com-2.png" alt="" />
-                        </div>
-                        <div className="col-lg-3 col-md-3 col-sm-4 col-6">
-                            <img src="https://i.ibb.co/C876GwC/pngwing-com-4.png" alt="" />
-                        </div>
-                        <div className="col-lg-3 col-md-3 col-sm-4 col-6">
-                            <img src="https://i.ibb.co/JR9cZVv/pngwing-com-3.png" alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
+    var settings = {
+        className: "center",
+        infinite: true,
+        centerPadding: "60px",
+        slidesToShow: 4,
+        swipeToSlide: true,
+        autoplay: true,
+        speed: 1000,
+        autoplaySpeed: 2000,
+        cssEase: "linear"
 
+    };
+    return (
+        <div className='container'>
+            <div className="row py-5">
+                <Slider {...settings}>
+                    <div className="col-lg-3 col-md-3 col-sm-4 col-6">
+                        <img className='img-fluid' src="https://i.ibb.co/Tghj6Zx/pngwing-com.png" alt="" />
+                    </div>
+                    <div className="col-lg-3 col-md-3 col-sm-4 col-6">
+                        <img className='img-fluid' src="https://i.ibb.co/5K26qMT/pngwing-com-2.png" alt="" />
+                    </div>
+                    <div className="col-lg-3 col-md-3 col-sm-4 col-6">
+                        <img className='img-fluid' src="https://i.ibb.co/C876GwC/pngwing-com-4.png" alt="" />
+                    </div>
+                    <div className="col-lg-3 col-md-3 col-sm-4 col-6">
+                        <img className='img-fluid' src="https://i.ibb.co/JR9cZVv/pngwing-com-3.png" alt="" />
+                    </div>
+                    <div className="col-lg-3 col-md-3 col-sm-4 col-6">
+                        <img className='img-fluid' src="https://i.ibb.co/Zd8x4cR/pngwing-com-5.png" alt="" />
+                    </div>
+                    <div className="col-lg-3 col-md-3 col-sm-4 col-6">
+                        <img className='img-fluid' src="https://i.ibb.co/gmjDphc/pngwing-com-6.png" alt="" />
+                    </div>
+                </Slider>
+            </div>
         </div>
     );
 };
