79a98c737ae3fb9c47b45bb2e27f83ec3169cccb
shahnmah <shahn.mah@gmail.com>
2022-05-14 16:51:49 +0600

    quantity update added

--- src/components/UpdateItem/UpdateItem.js
+++ src/components/UpdateItem/UpdateItem.js
@@ -1,24 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const UpdateItem = () => {
     const { itemId } = useParams()
     const [bus, setBus] = useState({})
-    // useEffect(()=>{
-    //     fetch(`http://localhost:3000/updateItem/${itemId}`)
-    //     .then(res => res.json())
-    //     .then(data => setBus(data))
-    // },[])
-    fetch(`http://localhost:5000/bus/${itemId}`, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
+    const [quantity, setQuantity] = useState(0);
+    const [reload, setReload] = useState(false)
+    useEffect(()=>{
+        fetch(`http://localhost:5000/bus/${itemId}`)
+        .then(res => res.json())
+        .then(data => setBus(data))
+    },[reload])
+    const handleUpdateQuantity = () =>{
+        const newQuantity = {quantity};
+        fetch(`http://localhost:5000/allbuses/${itemId}`, {
 
-    })
-        .then(response => response.json())
-        .then(data => setBus(data));
-
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body:JSON.stringify(newQuantity),
+        })
+        .then(res=> res.json())
+        .then(data => console.log(data))
+        setReload(!reload)
+    }
 
     return (
         <div className='container w-50'>
@@ -36,11 +42,13 @@ const UpdateItem = () => {
                             <div className='d-flex justify-content-between'>
                                 <button style={{height: '40px'}} className='btn btn-primary py-2 px-5'>Delivered</button>
                                 <div>
-                                    <input className='mb-2' type="number" name="quantity" id="" /> <br />
-                                    <button className='btn btn-primary'>Update Quantity</button>
+                                    <input onChange={(e) => setQuantity(e.target.value)} className='mb-2' type="number" name="quantity" id="" /> <br />
+                                    <button onClick={handleUpdateQuantity} className='btn btn-primary'>Update Quantity</button>
                                 </div>
                             </div>
-
+                            <div className='d-flex justify-content-center mt-4 mb-2'>
+                            <Link to='/manageItems' role='button' className='btn btn-primary py-2 px-5'>Manage All Buses</Link>
+                            </div>
                         </div>
                     </div>
                 </div>
